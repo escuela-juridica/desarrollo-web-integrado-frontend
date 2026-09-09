114484a28e674904c3cfda8831634aa5955a7eb6
Enrique Prada <enrique.pdg@gmail.com>
2026-09-09 02:51:43 -0500

    feat:cambio contraseña

--- src/app/features/auth/verificar-correo/verificar-correo.spec.ts
+++ src/app/features/auth/verificar-correo/verificar-correo.spec.ts
@@ -1,4 +1,7 @@
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
 import { VerificarCorreo } from './verificar-correo';
 
@@ -9,6 +12,7 @@ describe('VerificarCorreo', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [VerificarCorreo],
+      providers: [provideRouter([]), provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(VerificarCorreo);
